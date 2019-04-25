a7731a94d7a57ed4428e1a26e480b34ebf79ca5e
Hugo Abreu <hugoabreu90@gmail.com>
2019-04-25 21:55:41 +0100

    Project preparation

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+import flats from '../../data/flats';
+
+class App extends Component {
+  render() {
+    return(
+
+      // Things
+
+      );
+  }
+}
+
+export default App;
---
+++ src/components/flat_list.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+// Things
+
+export default //Thing ;
